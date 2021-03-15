9cc7ff18fac79418690f62201c1045fe313b2ee2
lihe <shilishihe@gmail.com>
2021-03-15 23:26:37 +0800

    作业二完成

--- test_calculator.py
+++ test_calculator.py
@@ -1,24 +1,25 @@
 import pytest
-from hw0307.testcase.calculator import Calculator
+import yaml
 
+from hw0307.testcase.calculator import Calculator
 
 
 class TestCal:
     def setup_class(self):
-        self.cal=Calculator()
+        self.cal = Calculator()
         print("\n开始测试！\n")
 
     def teardown_class(self):
         print("\n结束测试！")
 
-    @pytest.mark.parametrize("a,b,expected",[(3,5,8),(-1,-2,-3),(3000,1000,4000),],
-                         ids=["int","minus","bigint"])
-    def test_add(self,a,b,expected):
-        assert expected == self.cal.add(a,b)
+    @pytest.mark.parametrize("a,b,expected", [(3, 5, 8), (-1, -2, -3), (3000, 1000, 4000), ],
+                             ids=["int", "minus", "bigint"])
+    def test_add(self, a, b, expected):
+        assert expected == self.cal.add(a, b)
 
     @pytest.mark.parametrize("a,b,expected", [(9, 5, 4), (-3, -2, -1), (4000, 1000, 3000), ],
                              ids=["int", "minus", "bigint"])
-    def test_sub(self,a,b,expected):
+    def test_sub(self, a, b, expected):
         assert expected == self.cal.sub(a, b)
 
     @pytest.mark.parametrize("a,b,expected", [(2, 3, 6), (-3, -2, 6), (400, 100, 40000), ],
@@ -30,4 +31,3 @@ class TestCal:
                              ids=["int", "minus", "bigint"])
     def test_div(self, a, b, expected):
         assert expected == self.cal.div(a, b)
-
