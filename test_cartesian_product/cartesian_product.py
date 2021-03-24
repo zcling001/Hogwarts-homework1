050365cc9c6a8d6ab5c1f684b32de9f8d6a7d447
lihe <shilishihe@gmail.com>
2021-03-24 22:02:10 +0800

    1.提交笛卡尔积打印输出

--- test_cartesian_product/cartesian_product.py
+++ test_cartesian_product/cartesian_product.py
@@ -1,36 +1,48 @@
 import itertools
 
 
-class cartesian(object):
-    def __init__(self):
-        self._data_list = []
-
-    # 添加生成笛卡尔积的数据列表
-    def add_data(self, data=[]):
-        self._data_list.append(data)
-
-    # 计算笛卡尔积
-    def build(self):
-        for item in itertools.product(*self._data_list):
-            print(item)
-
-
-# 前5后6
-def runfunc0():
-    car = cartesian()
-    car.add_data([1, 2, 3, 4, 5])
-    car.add_data([6, 7, 8, 9, 10, 11])
-    car.build()
+# class cartesian(object):
+#     def __init__(self):
+#         self._data_list = []
+#
+#     # 添加生成笛卡尔积的数据列表
+#     def add_data(self, data=[]):
+#         self._data_list.append(data)
+#
+#     # 计算笛卡尔积
+#     def build(self):
+#         for item in itertools.product(*self._data_list):
+#             print(item)
 
 
-# 前6后5
-def runfunc1():
-    car = cartesian()
-    car.add_data([6, 7, 8, 9, 10, 11])
-    car.add_data([1, 2, 3, 4, 5])
-    car.build()
+def runfunc(array0, array1):
+
+    if len(array0) > len(array1):
+        longArray = array0
+        shortArray = array1
+    else:
+        longArray = array1
+        shortArray = array0
+
+    longArrayLenth = len(longArray)
+    shortArrayLenth = len(shortArray)
+
+    # for i in range(longArrayLenth):
+    #     for j in range(shortArrayLenth):
+    #         if i == j:
+    #             print('%d,%d' % (longArray[i], shortArray[i]))
+    #         else:
+    #             if i >= shortArrayLenth:
+    #                 print('%d,%d' % (longArray[i], shortArray[i - shortArrayLenth]))
+    #                 break
+
+    for i in range(longArrayLenth):
+        print('%d,%d' % (longArray[i], shortArray[i - shortArrayLenth]))
 
 
 if __name__ == "__main__":
-    runfunc0()
-    # runfunc1()
+    array0 = [1, 3, 5, 7, 9]
+    array1 = [2, 4, 6, 8]
+    # array0 = [2, 4, 6, 8]
+    # array1 = [1, 3, 5, 7, 9]
+    runfunc(array0, array1)
